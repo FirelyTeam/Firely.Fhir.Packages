eb70d432efbe895b7787354214dc8908128bac37
Martijn Harthoorn <m.harthoorn@furore.com>
2019-07-01 15:05:01 +0200

    change: rename canonical index file to .index.json

--- Hl7.Fhir.Packages/Constants/DiskNames.cs
+++ Hl7.Fhir.Packages/Constants/DiskNames.cs
@@ -4,7 +4,7 @@
     {
         public const string Manifest = "package.json";
         public const string PackageLockFile = "fhirpkg.lock.json";
-        public const string CanonicalIndexFile = "_index.json";
+        public const string CanonicalIndexFile = ".index.json";
         public const string PackageFolder = "package";
     }
 
