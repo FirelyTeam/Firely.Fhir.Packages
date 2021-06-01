2ece5a3f05830ca37ed796545bdb84bc18459b7f
martijn <martijn@fire.ly>
2021-06-01 09:00:38 +0200

    add: indexing for cache

---
+++ Firely.Fhir.Packages/Core/FileIndexExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Firely.Fhir.Packages
+{
+    public static class FileIndexExtensions
+    {
+        public static async Task Index(this FileIndex index, IPackageCache cache, PackageClosure closure)
+        {
+            foreach (var reference in closure.References)
+            {
+                await index.Index(cache, reference);
+            }
+        }
+
+        public static async Task Index(this FileIndex index, IPackageCache cache, PackageReference reference)
+        {
+            var idx = await cache.GetCanonicalIndex(reference);
+
+            index.Add(reference, idx);
+        }
+
+        public static void Add(this FileIndex index, PackageReference reference, IEnumerable<ResourceMetadata> cindex)
+        {
+            foreach (var item in cindex)
+            {
+                index.Add(reference, item);
+            }
+        }
+
+        public static void Add(this FileIndex index, PackageReference reference, CanonicalIndex cindex)
+        {
+            if (cindex.Files is object)
+            {
+                index.Add(reference, cindex.Files);
+            }
+        }
+
+        public static async Task Index(this FileIndex index, IProject project)
+        {
+            var entries = await project.GetIndex();
+
+            index.Add(PackageReference.None, entries);
+        }
+
+        internal static async Task AddToIndexIndex(this IPackageCache cache, FileIndex index, PackageReference reference)
+        {
+            var canonicals = await cache.GetCanonicalIndex(reference);
+            index.Add(reference, canonicals);
+        }
+    }
+}
