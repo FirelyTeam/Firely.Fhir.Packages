eefc72ea2f08679b8ab77f231c335be55c74f700
Rob5045 <rob@robertmark.nl>
2022-08-23 18:33:58 +0200

    PackageRestorer restore now accumulates errors into an AggregateException and includes package information whenavailable in a PackageRestoreException. Added unit test HandleInvalidPackagesOnRestore.

--- Firely.Fhir.Packages.Tests/Packaging.cs
+++ Firely.Fhir.Packages.Tests/Packaging.cs
@@ -1,8 +1,10 @@
 ﻿using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Firely.Fhir.Packages.Tests
 {
@@ -46,6 +48,66 @@ namespace Firely.Fhir.Packages.Tests
             package.Should().Contain(e => e.FilePath == @"package\other\.index.json");
 
 
+        }
+
+        [TestMethod]
+        [DataRow(true, 0)]
+        [DataRow(true, 1)]
+        [DataRow(true, 1)]
+        [DataRow(false, 0)]
+        [DataRow(false, 1)]
+        [DataRow(false, 1)]
+        public async Task HandleInvalidPackagesOnRestore(bool includeValidPackage, int invalidPackageCount)
+        {
+            // Arrange
+            const string packageName = "MyInvalidPackage";
+            const string packageRange = "invalid";
+
+            var dependencies = new List<string>();
+
+            if (includeValidPackage)
+                dependencies.Add(IndexGenerationTest.HL7_CORE_PACKAGE_R4);
+
+            for (int i = 0; i < invalidPackageCount; i++)
+                dependencies.Add($"{packageName}{i+1}@{i+1}.{packageRange}");
+
+            var fixtureDirectory = TestHelper.InitializeTemporary("integration-test", dependencies.ToArray()).Result;
+
+            // Act
+            try
+            {
+                // TestHelper.Open calls restore on package context
+                await TestHelper.Open(fixtureDirectory, _ => { });
+            }
+            catch (AggregateException aex)
+            {
+                if (invalidPackageCount > 0)
+                {
+                    // Assert
+                    aex.InnerExceptions.Should().HaveCount(invalidPackageCount);
+                    aex.Message.Should().Be($"One or more errors occurred. (Invalid version string: \"1.{packageRange}\")");
+
+                    for (int i = 0; i < invalidPackageCount; i++)
+                    {
+                        aex.InnerExceptions[i].Should().BeOfType<PackageRestoreException>();
+
+                        var prex = (PackageRestoreException)aex.InnerExceptions[i];
+
+                        prex.PackageDependencies.Should().HaveCount(1);
+
+                        var dependency = prex.PackageDependencies.Single();
+
+                        dependency.Name.Should().Be($"{packageName}{i + 1}");
+
+                        prex.Message.Should().Be($"Invalid version string: \"{i + 1}.{packageRange}\"");
+                    }
+
+                    return;
+                }
+            }
+
+            if (invalidPackageCount > 0)
+                Assert.Fail("Expected AggregateException to be thrown");
         }
 
         private static FileEntry createFileEntry(string content, string path) => new(path, Encoding.ASCII.GetBytes(content));
