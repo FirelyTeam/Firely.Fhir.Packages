0a3fca466b6b86dd6bfefd4a46c7601e9a18a110
mmsmits <marten@fire.ly>
2024-02-15 17:42:35 +0100

    fix super weird error

--- Firely.Fhir.Packages/Source/FhirPackageSource.cs
+++ Firely.Fhir.Packages/Source/FhirPackageSource.cs
@@ -95,7 +95,7 @@ namespace Firely.Fhir.Packages
                         yield return new PackageDependency(dep, "latest");
                     else
                     {
-                        var versionDep = "=" + splitDependency[1];
+                        var versionDep = splitDependency[1];
                         yield return new PackageDependency(splitDependency[0], versionDep);
                     }
                 }
