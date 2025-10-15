62e19013610752cac1cb38d59038586449f6efb4
mmsmits <8956842+mmsmits@users.noreply.github.com>
2025-10-15 11:06:09 +0200

    fix new lines for macs

--- Firely.Fhir.Packages.Tests/ManifestSerializationTest.cs
+++ Firely.Fhir.Packages.Tests/ManifestSerializationTest.cs
@@ -64,7 +64,7 @@ namespace Firely.Fhir.Packages.Tests
 
             json = PackageParser.SerializeManifest(manif);
 
-            json.Should().Contain("  \"author\": {\r\n    \"name\": \"Marten\",\r\n    \"email\": \"foo@bar.nl\",\r\n    \"url\": \"https://foo.bar\"\r\n  }");
+            json.Should().Contain("\"author\": {\n    \"name\": \"Marten\",\n    \"email\": \"foo@bar.nl\",\n    \"url\": \"https://foo.bar\"\n  }");
             manif2 = PackageParser.ParseManifest(json);
             manif2.Should().BeEquivalentTo(manif);
             manif2?.Author?.Should().Be("Marten <foo@bar.nl> (https://foo.bar)");
